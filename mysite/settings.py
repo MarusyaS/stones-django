8dabba55cc6a6896c986ce2b2a2d436d83a51878
Marusya <marusya.sysoeva@gmail.com>
2023-06-10 18:12:49 +0300

    for webserver

--- mysite/settings.py
+++ mysite/settings.py
@@ -36,8 +36,8 @@ SECRET_KEY = 'django-insecure-x!4krbn^n7qats3zx-6=%y*+u#fqd$hl6%__!2h_-d45yq85z8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
-# ALLOWED_HOSTS = ['sandbox.rssda.su']
+# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sandbox.rssda.su']
 
 CORS_ALLOW_ALL_ORIGINS = True
 
@@ -95,8 +95,8 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'stonelib/static/build/static'),  # update the STATICFILES_DIRS
 )
 
-# STATIC_ROOT = '/home/u187324/sandbox.rssda.su/www/static/'
-STATIC_ROOT = '/home/marsy/tmp'
+STATIC_ROOT = '/home/u187324/sandbox.rssda.su/www/static/'
+# STATIC_ROOT = '/home/marsy/tmp'
 # STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 WSGI_APPLICATION = 'mysite.wsgi.application'
 
